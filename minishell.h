ca5895404d78e71d483170596163c08672c5842b
Anna Semsey <asemsey@3-G-9.42heilbronn.de>
2024-02-19 10:32:10 +0100

    start

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 10:09:31 by asemsey           #+#    #+#             */
+/*   Updated: 2024/02/19 10:31:20 by asemsey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <signal.h>
+# include "include/readline-8.2/readline.h"
+
+#endif
