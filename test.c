9d9481027de3b97ae005832fcbdd430bd460a016
Fayeza Nikzad <fnikzad@1-C-4.42heilbronn.de>
2024-02-21 12:10:33 +0100

    asdasdA

--- test.c
+++ test.c
@@ -3,22 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/20 15:14:49 by asemsey           #+#    #+#             */
-/*   Updated: 2024/02/21 11:02:17 by asemsey          ###   ########.fr       */
+/*   Updated: 2024/02/21 12:09:36 by fnikzad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+
+
+void	execute_cd(char **args)
+{
+	char cwd[1024];
+	if (ft_strncmp(args[0], "pwd", 3) == 0)
+	{
+		if (getcwd(cwd, sizeof(cwd)) != 0)
+			printf("%s\n", cwd);
+	}
+	if (args[0][0] == 's' && args[0][1] == 'd')
+	{
+		if (args[1] == NULL)
+			chdir(getenv("HOME"));
+		else 
+			chdir("args[1]");
+	}
+}
+
 int	dosomething(char **argv)
 {
-	int		i;
+	// int		i;
 
-	i = 0;
-	while (argv[i])
-		ft_putstr_fd(argv[i++], 1);
+	// i = 0;
+	// while (argv[i])
+	// {
+	// 	ft_putstr_fd(argv[i++], 1);
+	// }
+	
+	execute_cd(argv);
 	return (1);
 }
 
@@ -41,7 +64,7 @@ int	prompt(void)
 	}
 	free(str);
 	free_all(args);
-	write(1, "\n", 1);
+	// write(1, "\n", 1);
 	return (1);
 }
 
