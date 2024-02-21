5edf17b608977d854fe8af2ebae5497e6fe5bbfc
Anna Semsey <asemsey@3-E-5.42heilbronn.de>
2024-02-21 14:41:50 +0100

    exit

--- executables.c
+++ executables.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executables.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fnikzad <fnikzad@student.42.fr>            +#+  +:+       +#+        */
+/*   By: asemsey <asemsey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 12:21:56 by fnikzad           #+#    #+#             */
-/*   Updated: 2024/02/21 13:41:52 by fnikzad          ###   ########.fr       */
+/*   Updated: 2024/02/21 14:40:04 by asemsey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,24 @@ int valid_command(char **args)
 {
 	if (ft_strcmp(args[0], "cd") == 0 || ft_strcmp(args[0], "pwd") == 0 || ft_strcmp(args[0], "echo") == 0 || ft_strcmp(args[0], "export") == 0)
         return 1;
-    if (ft_strcmp(args[0], "unset") == 0 || ft_strcmp(args[0], "env") == 0 || ft_strcmp(args[0], "exit") == 0)
+    if (ft_strcmp(args[0], "unset") == 0 || ft_strcmp(args[0], "env") == 0 || ft_strcmp(args[0], "exit") == 0 || ft_strcmp(args[0], "ls") == 0)
         return 1;
     return 0;
+}
+
+// void	execute_ls(char **args)
+// {
+// 	if (ft_strcmp(args[0], "ls") == 0)
+// 	{
+// 		if (!args[1])
+			
+// 	}
+// }
+
+void	execute_exit(char **args)
+{
+	if (ft_strcmp(args[0], "exit") == 0)
+		exit(EXIT_SUCCESS);
 }
 
 void	execute_pwd(char **args)
@@ -56,6 +71,7 @@ void	execute_all(char **args)
 		perror("command not valid");
 		return ;
 	}
+	execute_exit(args);
 	execute_pwd(args);
 	execute_cd(args);
 }
